ce831f1faac3c721a6022cd3329b8a2db0d489a8
tangxianfeng <511317747@qq.com>
2021-11-16 17:02:46 +0800

    代理测试

--- myapp/src/setupProxy.js
+++ myapp/src/setupProxy.js
@@ -1,12 +1,17 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/api',{
-        target:'http://chst.vip:666',
+    app.use(createProxyMiddleware('/api', {
+        target: 'http://chst.vip:666',
         pathRewrite: {
             "^/api": "/"
         },
-        changeOrigin: true
+        changeOrigin: true,
+        router: {
+            // 用于重写目标服务器
+            // 当请求的host为150.158.185.32时重写目标服务器为后面的地址
+            'http://150.158.185.32/api': 'http://chst.vip:666'
+        }
     }))
 
 };
