2747df7204cb1069887e51659314747f28867048
唐先锋 <511317747@qq.com>
2022-10-17 10:46:45 +0800

    update: https

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(createProxyMiddleware('/api', {
-        target: 'http://150.158.185.32/mua',
+        target: 'https://www.yuanaliu.com/mua',
         pathRewrite: {
             "^/api": "/"
         },
